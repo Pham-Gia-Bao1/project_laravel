b0eb51963a0a0851a09896a912fbf1d0b1b83bf8
Mai <mai.ho25@student.passerellesnumeriques.org>
2024-03-25 10:28:39 +0700

    Fix conflict

--- resources/views/Layout/subNavBar.blade.php
+++ resources/views/Layout/subNavBar.blade.php
@@ -1,10 +1,14 @@
 <div class="top-act">
     <div class="top-act__group d-md-none top-act__group--single">
 
-        <button class="top-act__btn">
-            <input type="text" class="bg-dark" id="searchInput">
-            <img src="./assets/icons/search.svg" alt="" id="icon-serch" class="icon top-act__icon rounded-circle" />
-        </button>
+        <form action="home" method="get">
+                
+            <div class="top-act__btn">
+                <input type="text" class="bg-dark" id="searchInput" name="search" placeholder="Search...">
+                <button type="submit"><img src="./assets/icons/search.svg" alt="" id="icon-serch" class="icon top-act__icon rounded-circle" /></button>
+            </div>
+
+            </form>
 
     </div>
 
