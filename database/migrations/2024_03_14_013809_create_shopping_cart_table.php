a25daed75a4cf6e98d79192947851a87a40cdf71
PhamGiaBao <phamgiabao123abc@gmail.com>
2024-04-02 08:14:35 +0700

    finished fix conflic

--- database/migrations/2024_03_14_013809_create_shopping_cart_table.php
+++ database/migrations/2024_03_14_013809_create_shopping_cart_table.php
@@ -18,10 +18,6 @@ return new class extends Migration
             $table->unsignedBigInteger('user_id'); // Thêm trường user_id
             $table->unsignedBigInteger('product_id'); // Thêm trường user_id
             $table->timestamps();
-            // Cột khóa ngoại
-            // $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
-            // $table->foreign('product_id')->references('id')->on('products')->onDelete('cascade');
-
         });
     }
 
